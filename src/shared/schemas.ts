b421374058e26e0ded3a3fff1ece8a0913784d41
Krio <uo265710@uniovi.es>
2021-07-15 18:46:41 +0200

    First definition of subscription event

--- src/shared/schemas.ts
+++ src/shared/schemas.ts
@@ -1,7 +1,13 @@
 /**
  * Schema with data to perform subscriptions 
  */
-export interface SubscriptionMessage {
+export interface SubscriptionRequest {
 	room_code: string,
+	user_name: string
+}
+
+
+export interface SubscriptionNotification {
+	timestamp: number,
 	user_name: string
 }
